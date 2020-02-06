210a3184ae7e21e83818112190616febcd6751f0
DanielLengler <theultimit.asis@gmail.com>
2020-02-06 13:54:31 -0500

    Main page buttons and listeners setup.

--- app/src/main/java/com/asis/finalproject/MainActivity.java
+++ app/src/main/java/com/asis/finalproject/MainActivity.java
@@ -1,6 +1,7 @@
 package com.asis.finalproject;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton bbcNewsReaderButton = findViewById(R.id.bbcNewsReaderButton);
+        bbcNewsReaderButton.setOnClickListener((v) -> {
+
+        });
+
+        ImageButton guardianArticleSearchButton = findViewById(R.id.guardianArticleSearchButton);
+        guardianArticleSearchButton.setOnClickListener((v) -> {
+
+        });
+
+        ImageButton nasaEarthImageButton = findViewById(R.id.nasaEarthImageButton);
+        nasaEarthImageButton.setOnClickListener((v) -> {
+
+        });
+
+        ImageButton nasaImageOfDayButton = findViewById(R.id.nasaImageOfDayButton);
+        nasaImageOfDayButton.setOnClickListener((v) -> {
+
+        });
     }
 }
