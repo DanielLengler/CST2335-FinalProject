1cf1e10a3ddc1965378100b05af08535103f3bcb
DanielLengler <theultimit.asis@gmail.com>
2020-03-12 15:45:23 -0400

    Fixing previous commit

---
+++ app/src/main/java/com/asis/finalproject/MainActivity.java
@@ -0,0 +1,35 @@
+package com.asis.finalproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.asis.finalproject.nasaearthimage.NasaImageSelectorActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton bbcNewsReaderButton = findViewById(R.id.bbcNewsReaderButton);
+        bbcNewsReaderButton.setOnClickListener((v) -> {
+
+        });
+
+        ImageButton guardianArticleSearchButton = findViewById(R.id.guardianArticleSearchButton);
+        guardianArticleSearchButton.setOnClickListener((v) -> {
+
+        });
+
+        ImageButton nasaEarthImageButton = findViewById(R.id.nasaEarthImageButton);
+        nasaEarthImageButton.setOnClickListener((v) -> startActivity(new Intent(MainActivity.this, NasaImageSelectorActivity.class)));
+
+        ImageButton nasaImageOfDayButton = findViewById(R.id.nasaImageOfDayButton);
+        nasaImageOfDayButton.setOnClickListener((v) -> {
+        });
+    }
+}
