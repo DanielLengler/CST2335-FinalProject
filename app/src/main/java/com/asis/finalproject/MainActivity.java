ee52d6161360e0e686df07c3bca87e04a5b4ab05
Asis <47801367+Asis2019@users.noreply.github.com>
2020-03-15 19:15:27 -0400

    Updated master to comply with final project requirements and new gradle version

--- app/src/main/java/com/asis/finalproject/MainActivity.java
+++ app/src/main/java/com/asis/finalproject/MainActivity.java
@@ -1,35 +1,127 @@
 package com.asis.finalproject;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton bbcNewsReaderButton = findViewById(R.id.bbcNewsReaderButton);
-        bbcNewsReaderButton.setOnClickListener((v) -> {
+        setupActionBarAndDrawer();
 
-        });
+        ImageButton bbcNewsReaderButton = findViewById(R.id.bbcNewsReaderButton);
+        bbcNewsReaderButton.setOnClickListener((v) -> launchBBC());
 
         ImageButton guardianArticleSearchButton = findViewById(R.id.guardianArticleSearchButton);
-        guardianArticleSearchButton.setOnClickListener((v) -> {
-
-        });
+        guardianArticleSearchButton.setOnClickListener((v) -> launchGuardian());
 
         ImageButton nasaEarthImageButton = findViewById(R.id.nasaEarthImageButton);
-        nasaEarthImageButton.setOnClickListener((v) -> {
-
-        });
+        nasaEarthImageButton.setOnClickListener((v) -> launchNasaEarthImage());
 
         ImageButton nasaImageOfDayButton = findViewById(R.id.nasaImageOfDayButton);
-        nasaImageOfDayButton.setOnClickListener((v) -> {
+        nasaImageOfDayButton.setOnClickListener((v) -> launchNasaImageOfTheDay());
+    }
 
-        });
+    private void setupActionBarAndDrawer() {
+        //For ToolBar:
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //For NavigationDrawer:
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.actionLaunchBBC:
+                launchBBC();
+                break;
+            case R.id.actionLaunchGuardian:
+                launchGuardian();
+                break;
+            case R.id.actionLaunchNasaEarthImage:
+                launchNasaEarthImage();
+                break;
+            case R.id.actionLaunchNasaImageOfDay:
+                launchNasaImageOfTheDay();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.actionLaunchBBC:
+                launchBBC();
+                break;
+            case R.id.actionLaunchGuardian:
+                launchGuardian();
+                break;
+            case R.id.actionLaunchNasaEarthImage:
+                launchNasaEarthImage();
+                break;
+            case R.id.actionLaunchNasaImageOfDay:
+                launchNasaImageOfTheDay();
+                break;
+        }
+        return true;
+    }
+
+    /*
+    * Above this line is code for the navigation drawer and toolbars.
+    * You should not need to touch any of it.
+    * --------------------------------------------------------------
+    * In the methods below go your startActivity code for launching your
+    * particular part of the project.
+    * */
+
+    /**
+     * Starts the BBC News activity
+     */
+    private void launchBBC() {
+
+    }
+
+    /**
+     * Starts the Guardian News activity
+     */
+    private void launchGuardian() {
+
+    }
+
+    /**
+     * Starts the Nasa earth image activity
+     */
+    private void launchNasaEarthImage() {
+
+    }
+
+    /**
+     * Starts the Nasa image of the day activity
+     */
+    private void launchNasaImageOfTheDay() {
+
     }
 }
